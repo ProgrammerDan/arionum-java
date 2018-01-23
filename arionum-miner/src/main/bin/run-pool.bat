a5e5df5ff64576ced9aaf0f7aee44845aa359b3f
Daniel Boston <programmerdan@gmail.com>
2018-01-22 21:34:56 -0500

    Quick fix for run-pool
    so that it'll generate properly

--- arionum-miner/src/main/bin/run-pool.bat
+++ arionum-miner/src/main/bin/run-pool.bat
@@ -1,1 +1,1 @@
-java -jar target\arionum-miner-0.0.9-SNAPSHOT.jar pool http://aropool.com/ [your_arionum_wallet_adress] [# of threads]
+java -jar target\@batchJarName@.jar pool http://aropool.com/ %1
