682fed65208c62dac5c70fea5d35ca9e72efba62
ProgrammerDan <programmerdan@gmail.com>
2018-02-05 04:47:01 -0500

    Adding ez support for building argon2i slimlib in linux

--- arionum-miner/src/main/bin/build-argon.sh
+++ arionum-miner/src/main/bin/build-argon.sh
@@ -8,10 +8,16 @@ git clone git://github.com/ProgrammerDan/phc-winner-argon2 arionum-argon
 
 cd arionum-argon
 
+git pull
+
 git checkout arionum-argon2i
 
 make clean && CFLAGS="-m64" OPTTARGET=native NO_THREADS=1 make
 
 cp libargon2.so.1 libargon2.so
 
-echo "#! /bin/bash \n\n java -Djna.library.path=\"`pwd`\" -jar arionum-miner-java.jar" > ../run.sh 
+echo "#! /bin/bash
+java -Djna.library.path=\"`pwd`\" -jar arionum-miner-java.jar" > ../run.sh 
+
+chmod +x ../run.sh
+
